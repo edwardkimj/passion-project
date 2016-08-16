d33096c0ef3a381dd9b1e4a72a237323e71a6dc9
Edward Kim <edwardjkim@Edwards-MacBook-Air-3.local>
2016-08-15 17:47:45 -0700

    create login and register forms

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,19 @@
+get '/sessions/new' do
+  erb :'sessions/new'
+end
+
+post '/sessions' do
+  @user = User.find_by_email(params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:id] = @user.id
+    redirect "users/profile"
+  else
+    @errors = ["Wrong Login! Please try again!"]
+    erb :'users/new'
+  end
+end
+
+delete '/sessions' do
+  session[:id] = nil
+  redirect '/'
+end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,14 +1,28 @@
 get '/users' do
+  @users = User.all
+  erb :'/users/index'
 end
 
 get '/users/new' do
-end
-
-post '/users' do
+  erb :'users/new'
 end
 
 get '/users/:id' do
+  @user = current_user
+  @clients = current_user.clients
 end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect "/users/profile"
+  else
+    @errors = @users.errors.full_messages
+    erb :'users/new'
+  end
+end
+
 
 get '/users/:id/edit' do
 end
